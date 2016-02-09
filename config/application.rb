35b76588d4b6f04cd07a232cb6e335ebcbbc7971
brandon in <brandonin@me.com>
2016-02-09 09:29:57 -0800

    heroku version

--- config/application.rb
+++ config/application.rb
@@ -54,6 +54,6 @@ module Prelaunchr
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
     
-    config.ended = true
+    config.ended = false
   end
 end
